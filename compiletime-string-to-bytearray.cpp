0e9163c79a71e66af86a9de5e8a8b32319f6dbfd
Gabriel <51248378+cristeigabriel@users.noreply.github.com>
2021-05-04 20:39:24 +0300

    Improve code, comments

--- compiletime-string-to-bytearray.cpp
+++ compiletime-string-to-bytearray.cpp
@@ -5,13 +5,27 @@
 #include <string>
 #include <array>
 
-template <char token, typename T, size_t N = sizeof( T )>
-constexpr size_t _count_token( T &&str )
+#define DELIMITING_WILDCARD ' '
+
+//	@sean :)
+constexpr int _char_to_int( char ch )
+{
+	if( ch >= '0' && ch <= '9' )
+		return ch - '0';
+
+	if( ch >= 'A' && ch <= 'F' )
+		return ch - 'A' + 10;
+
+	return ch - 'a' + 10;
+};
+
+template <char wildcard, typename T, size_t N = sizeof( T )>
+constexpr size_t _count_wildcard( T &&str )
 {
 	size_t count = 1u;
 	for( const auto &character : str )
 	{
-		if( character == token )
+		if( character == wildcard )
 		{
 			++count;
 		}
@@ -20,44 +34,43 @@ constexpr size_t _count_token( T &&str )
 	return count;
 }
 
+//	construct a base16 hex and emplace it at make_count
+//	change 16 to 256 if u want the result to be when:
+//	sig[0] == 0xA && sig[1] == 0xB = 0xA0B
+//	or leave as is for the scenario to return 0xAB
+#define CONCATE_HEX_FACTOR 16
+#define CONCATE_HEX(a, b) ( CONCATE_HEX_FACTOR * ( a ) + ( b ) )
+
 template
 <   char skip_wildcard,
 	//	How many occurances of a delimiting wildcard do we find in sig
-	size_t CT,
+	size_t delimiter_count,
 	typename T, size_t N = sizeof( T )>
 	constexpr auto _make_array( T &&sig )
 {
-	//	@sean :)
-	constexpr auto char_to_int = []( char input ) -> int
-	{
-		if( input >= '0' && input <= '9' )
-			return input - '0';
+	static_assert( delimiter_count > 0, "this is a logical error, delimiter count can't be of size 0" );
+	static_assert( N > 1, "sig length must be bigger than 1" );
 
-		if( input >= 'A' && input <= 'F' )
-			return input - 'A' + 10;
+	//	Resulting byte array, for delimiter_count skips we should have delimiter_count integers
+	std::array<int, delimiter_count> ret{};
 
-		return input - 'a' + 10;
-	};
-
-	//	Resulting byte array, for CT skips we should have CT integers
-	std::array<int, CT> ret{};
-
-	//	List of skips that point to the position of the wildcard in skip
-	std::array<int, CT> skips{};
+	//	List of skips that point to the position of the delimiter wildcard in skip
+	std::array<size_t, delimiter_count> skips{};
 
 	//	Current skip
 	size_t skip_count = 0u;
 
 	//	Character count, traversed for skip
-	size_t character_count = 0u;
-	for( int i = 0; i < N; ++i )
+	size_t skip_traversed_character_count = 0u;
+	for( size_t i = 0u; i < N; ++i )
 	{
-		if( sig[i] == ' ' )
+		if( sig[i] == DELIMITING_WILDCARD )
 		{
-			skips[skip_count] = character_count;
+			skips[skip_count] = skip_traversed_character_count;
 			++skip_count;
 		}
-		++character_count;
+
+		++skip_traversed_character_count;
 	}
 
 	//	Finally traversed character count
@@ -67,44 +80,36 @@ template
 	size_t make_count = 1u;
 
 	//	Traverse signature
-	for( int i = 0; i < N; ++i )
+	for( size_t i = 0u; i < N; ++i )
 	{
 		//	Read before
-		if( i == 0 )
+		if( i == 0u )
 		{
 			//	We don't care about this, and we don't want to use 0
-			if( sig[0] == skip_wildcard )
+			if( sig[0u] == skip_wildcard )
 			{
-				ret[0] = -1;
+				ret[0u] = -1;
 				continue;
 			}
 
-			//	construct a base16 hex and emplace it at make_count
-			//	change 16 to 256 if u want the result to be when:
-			//	sig[0] == 0xA && sig[1] == 0xB = 0xA0B
-			//	or leave as is for the scenario to return 0xAB
-			ret[0] = 16 * char_to_int( sig[0] ) + char_to_int( sig[1] );
+			ret[0u] = CONCATE_HEX( _char_to_int( sig[0u] ), _char_to_int( sig[1u] ) );
 			continue;
 		}
 
 		//	Make result by skip data
 		for( const auto &skip : skips )
 		{
-			if( ( skip == i /*&& !computed_skips[skip]*/ ) && skip < N - 1 )
+			if( ( skip == i ) && skip < N - 1u )
 			{
 				//	We don't care about this, and we don't want to use 0
-				if( sig[i + 1] == skip_wildcard )
+				if( sig[i + 1u] == skip_wildcard )
 				{
 					ret[make_count] = -1;
 					++make_count;
 					continue;
 				}
 
-				//	construct a base16 hex and emplace it at make_count
-				//	change 16 to 256 if u want the result to be when:
-				//	sig[0] == 0xA && sig[1] == 0xB = 0xA0B
-				//	or leave as is for the scenario to return 0xAB
-				ret[make_count] = 16 * char_to_int( sig[i + 1] ) + char_to_int( sig[i + 2] );
+				ret[make_count] = CONCATE_HEX( _char_to_int( sig[i + 1u] ), _char_to_int( sig[i + 2u] ) );
 				++make_count;
 			}
 		}
@@ -113,7 +118,21 @@ template
 	return ret;
 }
 
-#define BUILD_ARRAY(a) _make_array<'?', _count_token<' '>( std::move( a ) )>( std::move( a ) )
+#define SKIP_WILDCARD '?'
+#define BUILD_ARRAY(a) _make_array<SKIP_WILDCARD, _count_wildcard<DELIMITING_WILDCARD>( a )>( a )
+#define BUILD_ARRAY_MV(a) _make_array<SKIP_WILDCARD, _count_wildcard<DELIMITING_WILDCARD>( std::move( a ) )>( std::move( a ) )
+
+//	-----
+//	usage
+//	-----
+template <int n>
+constexpr int combine_two()
+{
+	constexpr auto numbers = BUILD_ARRAY( "55 8B EC 83 E4 F8 8B 4D 08 BA ? ? ? ? E8 ? ? ? ? 85 C0 75 12 ?" );
+	constexpr int number = numbers[0];
+	constexpr int number_now = n + number;
+	return number_now;
+}
 
 int main()
 {
@@ -122,4 +141,10 @@ int main()
 	{
 		printf( "%x ", hex );
 	}
+
+	combine_two<3>();
+	constexpr auto saaahhah = combine_two<3>();
+	static_assert( combine_two<3>() == 88 );
+	static_assert( combine_two<3>() == saaahhah );
+	printf( "\n%d", saaahhah );
 }
