099cf7f1a7b23de07297ae70abb3572d7aac4fb0
Gabriel <51248378+cristeigabriel@users.noreply.github.com>
2021-05-05 21:06:59 +0300

    Heavily improve code

--- compiletime-string-to-bytearray.cpp
+++ compiletime-string-to-bytearray.cpp
@@ -1,150 +1,208 @@
 #pragma once
 
-#include <memory>
 #include <iostream>
-#include <string>
+#include <memory>
 #include <array>
+#include <utility>
+#include <string>
 
-#define DELIMITING_WILDCARD ' '
-
-//	@sean :)
-constexpr int _char_to_int( char ch )
+namespace stoba
 {
-	if( ch >= '0' && ch <= '9' )
-		return ch - '0';
-
-	if( ch >= 'A' && ch <= 'F' )
-		return ch - 'A' + 10;
-
-	return ch - 'a' + 10;
-};
-
-template <char wildcard, typename T, size_t N = sizeof( T )>
-constexpr size_t _count_wildcard( T &&str )
-{
-	size_t count = 1u;
-	for( const auto &character : str )
+	template <size_t N>
+	struct ct_string
 	{
-		if( character == wildcard )
+		constexpr ct_string( const char *str )
 		{
-			++count;
-		}
-	}
-
-	return count;
-}
-
-//	construct a base16 hex and emplace it at make_count
-//	change 16 to 256 if u want the result to be when:
-//	sig[0] == 0xA && sig[1] == 0xB = 0xA0B
-//	or leave as is for the scenario to return 0xAB
-#define CONCATE_HEX_FACTOR 16
-#define CONCATE_HEX(a, b) ( CONCATE_HEX_FACTOR * ( a ) + ( b ) )
-
-template
-<   char skip_wildcard,
-	//	How many occurances of a delimiting wildcard do we find in sig
-	size_t delimiter_count,
-	typename T, size_t N = sizeof( T )>
-	constexpr auto _make_array( T &&sig )
-{
-	static_assert( delimiter_count > 0, "this is a logical error, delimiter count can't be of size 0" );
-	static_assert( N > 1, "sig length must be bigger than 1" );
-
-	//	Resulting byte array, for delimiter_count skips we should have delimiter_count integers
-	std::array<int, delimiter_count> ret{};
-
-	//	List of skips that point to the position of the delimiter wildcard in skip
-	std::array<size_t, delimiter_count> skips{};
-
-	//	Current skip
-	size_t skip_count = 0u;
-
-	//	Character count, traversed for skip
-	size_t skip_traversed_character_count = 0u;
-	for( size_t i = 0u; i < N; ++i )
-	{
-		if( sig[i] == DELIMITING_WILDCARD )
-		{
-			skips[skip_count] = skip_traversed_character_count;
-			++skip_count;
-		}
-
-		++skip_traversed_character_count;
-	}
-
-	//	Finally traversed character count
-	size_t traversed_character_count = 0u;
-
-	//	Make count (we will supposedly have at least an instance in our return array)
-	size_t make_count = 1u;
-
-	//	Traverse signature
-	for( size_t i = 0u; i < N; ++i )
-	{
-		//	Read before
-		if( i == 0u )
-		{
-			//	We don't care about this, and we don't want to use 0
-			if( sig[0u] == skip_wildcard )
+			for( auto i = 0u; i != N; ++i )
 			{
-				ret[0u] = -1;
-				continue;
+				content[i] = str[i];
+			}
+		}
+
+		constexpr operator char const *( ) const
+		{
+			return content;
+		}
+
+		constexpr auto get() const
+		{
+			return operator const char *( );
+		}
+
+		char content[N + 1]{};
+	};
+
+	template<size_t N>
+	ct_string( char const ( & )[N] )->ct_string<N - 1>;
+
+	constexpr size_t _str_length( const char *str )
+	{
+		return *str ? 1u + _str_length( str + 1u ) : 0u;
+	}
+
+	//	Returns the occurences of a wildcard in a string alongside with the string
+	template <char wildcard, ct_string str>
+	constexpr auto _grab_string_data()
+	{
+		auto count = 1u;
+
+		for( auto i = 0u; i < _str_length( str.get() ); ++i )
+		{
+			if( str.get()[i] == wildcard )
+			{
+				++count;
+			}
+		}
+
+		return std::make_pair( count, str.get() );
+	}
+
+	//	@sean :)
+	template <typename T>
+	constexpr int _char_to_int( const T &ch )
+	{
+		if( ch >= '0' && ch <= '9' )
+			return ch - '0';
+
+		if( ch >= 'A' && ch <= 'F' )
+			return ch - 'A' + 10;
+
+		return ch - 'a' + 10;
+	};
+
+	//	construct a base16 hex and emplace it at make_count
+	//	change 16 to 256 if u want the result to be when:
+	//	sig[0] == 0xa && sig[1] == 0xb = 0xa0b
+	//	or leave as is for the scenario to return 0xab
+	template <typename T, T F = 16>
+	constexpr auto _concate_hex( const T &a, const T &b )
+	{
+		return T( F ) * a + b;
+	}
+
+	struct detail
+	{
+		constexpr static auto delimiting_wildcard = ' ';
+		constexpr static auto skip_wildcard = '?';
+		constexpr static auto hex_factor = 16;
+	};
+
+	template <class Detail>
+	struct data
+	{
+		template <ct_string str>
+		constexpr static auto get()
+		{
+			return _grab_string_data<Detail::delimiting_wildcard, str>();
+		}
+	};
+
+	template <ct_string str>
+	constexpr auto _generate_data()
+	{
+		return data<detail>::get<str>();
+	}
+
+	template <ct_string str>
+	constexpr auto make()
+	{
+		constexpr auto result = _generate_data<str>();
+		constexpr auto delimiter_count = result.first;
+		constexpr auto sig = result.second;
+		constexpr auto sig_length = _str_length( sig );
+
+		static_assert( delimiter_count > 1, "delimiter count must be bigger than 1" );
+		static_assert( sig_length > 0, "sig length must be bigger than 0" );
+
+		//	resulting byte array, for delimiter_count skips we should have delimiter_count integers
+		std::array<int, delimiter_count> ret{};
+
+		//	list of skips that point to the position of the delimiter wildcard in skip
+		std::array<size_t, delimiter_count> skips{};
+
+		//	current skip
+		auto skip_count = 0u;
+
+		//	character count, traversed for skip
+		auto skip_traversed_character_count = 0u;
+		for( auto i = 0u; i < sig_length; ++i )
+		{
+			if( sig[i] == detail::delimiting_wildcard )
+			{
+				skips[skip_count] = skip_traversed_character_count;
+				++skip_count;
 			}
 
-			ret[0u] = CONCATE_HEX( _char_to_int( sig[0u] ), _char_to_int( sig[1u] ) );
-			continue;
+			++skip_traversed_character_count;
 		}
 
-		//	Make result by skip data
-		for( const auto &skip : skips )
+		//	finally traversed character count
+		auto traversed_character_count = 0u;
+
+		//	make count (we will supposedly have at least an instance in our return array)
+		auto make_count = 1u;
+
+		//	traverse signature
+		for( auto i = 0u; i < sig_length; ++i )
 		{
-			if( ( skip == i ) && skip < N - 1u )
+			if( i == 0u )
 			{
-				//	We don't care about this, and we don't want to use 0
-				if( sig[i + 1u] == skip_wildcard )
+				//	we don't care about this, and we don't want to use 0
+				if( sig[0] == detail::skip_wildcard )
 				{
-					ret[make_count] = -1;
-					++make_count;
+					ret[0] = -1;
 					continue;
 				}
 
-				ret[make_count] = CONCATE_HEX( _char_to_int( sig[i + 1u] ), _char_to_int( sig[i + 2u] ) );
-				++make_count;
+				ret[0] = _concate_hex<int, detail::hex_factor>( _char_to_int( sig[0] ), _char_to_int( sig[1] ) );
+				continue;
+			}
+
+			//	make result by skip data
+			for( const auto &skip : skips )
+			{
+				if( ( skip == i ) && skip < sig_length - 1u )
+				{
+					//	we don't care about this, and we don't want to use 0
+					if( sig[i + 1] == detail::skip_wildcard )
+					{
+						ret[make_count] = -1;
+						++make_count;
+						continue;
+					}
+
+					ret[make_count] = _concate_hex<int, detail::hex_factor>( _char_to_int( sig[i + 1] ), _char_to_int( sig[i + 2] ) );
+					++make_count;
+				}
 			}
 		}
+
+		return ret;
 	}
-
-	return ret;
 }
-
-#define SKIP_WILDCARD '?'
-#define BUILD_ARRAY(a) _make_array<SKIP_WILDCARD, _count_wildcard<DELIMITING_WILDCARD>( a )>( a )
-#define BUILD_ARRAY_MV(a) _make_array<SKIP_WILDCARD, _count_wildcard<DELIMITING_WILDCARD>( std::move( a ) )>( std::move( a ) )
 
 //	-----
 //	usage
 //	-----
-template <int n>
+
+template <int N>
 constexpr int combine_two()
 {
-	constexpr auto numbers = BUILD_ARRAY( "55 8B EC 83 E4 F8 8B 4D 08 BA ? ? ? ? E8 ? ? ? ? 85 C0 75 12 ?" );
+	constexpr auto numbers = stoba::make<"55 8B EC 83 E4 F8 8B 4D 08 BA ? ? ? ? E8 ? ? ? ? 85 C0 75 12 ?">();
 	constexpr int number = numbers[0];
-	constexpr int number_now = n + number;
+	constexpr int number_now = N + number;
 	return number_now;
 }
 
 int main()
 {
-	constexpr auto shit = BUILD_ARRAY( "?? AA BB CC DD ? ? ? 02 31 32" );
-	for( const auto &hex : shit )
+	constexpr static auto result = combine_two<3>();
+	static_assert( result == 88 );
+	printf( "%d\n", result );
+
+	constexpr auto numbers = stoba::make<"55 8B EC 83 E4 F8 8B 4D 08 BA ? ? ? ? E8 ? ? ? ? 85 C0 75 12 ?">();
+	for( const auto &hex : numbers )
 	{
 		printf( "%x ", hex );
 	}
-
-	combine_two<3>();
-	constexpr auto saaahhah = combine_two<3>();
-	static_assert( combine_two<3>() == 88 );
-	static_assert( combine_two<3>() == saaahhah );
-	printf( "\n%d", saaahhah );
 }
